1c0708b736b7e2b4307e503aa0b8dbf838c6dee3
heliumyc <ericyc4@qq.com>
2019-01-04 20:34:00 +0800

    add name field to user

---
+++ src/main/java/com/bookexchange/app/model/context/dataWrapper/UserInfoDataWrapper.java
@@ -0,0 +1,25 @@
+package com.bookexchange.app.model.context.dataWrapper;
+
+import com.bookexchange.app.model.model.BookWithContactDO;
+import com.bookexchange.app.model.model.UserDO;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+public class UserInfoDataWrapper {
+    @JsonProperty("name")
+    private @Getter @Setter String name;
+    @JsonProperty("contact")
+    private @Getter @Setter String contact;
+    @JsonProperty("uid")
+    private @Getter @Setter int uid;
+
+    public UserInfoDataWrapper() {}
+
+    public UserInfoDataWrapper(UserDO userDO) {
+        this.name = userDO.getName();
+        this.contact = userDO.getContact();
+        this.uid = userDO.getUid();
+    }
+
+}
