da2b29f2456e3e477e6498d02afe19e72d7ae552
heliumyc <ericyc4@qq.com>
2019-02-17 13:31:06 +0800

    changes in deliver script

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,10 +1,10 @@
-set -x
 NAME = `gradle printProjectName`
-set +x
 
-set -x
 VERSION = `gradle printProjectVersion`
-set +x
 
 echo $NAME
 echo $VERSION
+echo $PWD
+
+echo 'run service'
+java -cp ./build/libs -jar ./build/libs/$NAME-$VERSION.jar
